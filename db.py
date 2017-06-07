c03fd895a630421bc5f247078a4af8851ceef684
corentin leclere <leclere.corentin@gmail.com>
2017-06-07 15:25:57 +0200

    commit de 07_06_2017_152557_09

--- db.py
+++ db.py
@@ -996,7 +996,7 @@ class bibliothecaire_dba (object):
                                                         datetime(planning.debut_poste, 'start of day', 'weekday 0')) as annee,
                                                  strftime('%m', datetime(planning.debut_poste, 'start of day', 'weekday 0')) as mois, 
                                                  ( strftime('%j', datetime(planning.debut_poste, 'start of day', '-3 days', 'weekday 4')) - 1 ) / 7 + 1 as semaine, 
-                                                 round( SUM( (JulianDay(fin_poste) - JulianDay(debut_poste)) * 24 ), 2 )as heure_semaine_travaillees
+                                                 round( SUM( (JulianDay(fin_poste) - JulianDay(debut_poste)) * 24 ), 2 )as heure_semaine_CP
                                                  FROM 
                                                     planning 
                                                 WHERE 
