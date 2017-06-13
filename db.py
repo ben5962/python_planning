d5f94451235a7f9778e08d911f1430a4c1ae5301
corentin leclere <leclere.corentin@gmail.com>
2017-06-13 13:54:20 +0200

    commit de 13_06_2017_135419_74

--- db.py
+++ db.py
@@ -805,6 +805,10 @@ leurs contraintes"""
             self.getBibliothecaireDba()
             .getRequeteCreaByName('vue_heures_annu_janjan')
             )
+        self.getCnx().execute(
+            self.getBibliothecaireDba()
+            .getRequeteCreaByName('vue_cumul_annu_juin_a_mai')
+            )
         
         
         log.debug("schemas doit maintenant etre cree")
@@ -1173,6 +1177,42 @@ class bibliothecaire_dba (object):
                                                 ;
 
                                                     """)
+            self.dicorequetes['crea'].setdefault('vue_cumul_annu_juin_a_mai',
+                                                    """create view
+                                                        'vue_cumul_annu_juin_a_mai'
+                                                            as
+                                                            select 
+                                                            sem_annu_juin_a_mai.annee_annu as annee_annu, 
+                                                            sem_annu_juin_a_mai.mois as mois ,
+                                                            sum(sem_annu_juin_a_mai.heure_semaine_travaillees) as vol_annu
+                                                            from 
+
+                                                            -- fonctionne. il faut grouper par annee_annu maintenant
+                                                            (SELECT 
+                                                            CAST(strftime('%Y', datetime(jour_travaille))  AS INTEGER) as repere,
+                                                            CAST(strftime('%m', datetime(jour_travaille))  AS INTEGER) > 5 as bool,
+                                                            strftime('%Y', datetime(jour_travaille)) as resultat_si_vrai,
+                                                            strftime('%Y', datetime(jour_travaille)) - 1 as resultat_si_faux,
+
+                                                            CASE 
+                                                                    WHEN cast(strftime('%m', datetime(jour_travaille)) as INTEGER) > 5 THEN strftime('%Y', datetime(jour_travaille))
+                                                                    ELSE strftime('%Y', datetime(jour_travaille, '-1 year'))
+                                                            END annee_annu, 
+
+                                                            strftime('%Y', datetime(jour_travaille))
+                                                                    as annee, 
+                                                            strftime('%m', datetime(jour_travaille)) as mois, 
+                                                            ( strftime('%j', datetime(jour_travaille, 'start of day', '-3 days', 'weekday 4')) - 1 ) / 7 + 1 
+                                                            as semaine,
+                                                            datetime(jour_travaille, 'start of day', '-3 days', 'weekday 4') as debut_semaine,
+                                                             round( SUM( (JulianDay(fin_periode) - JulianDay(debut_periode)) * 24 ) )
+                                                             as heure_semaine_travaillees 
+                                                            FROM periodes_travaillees 
+                                                            GROUP BY annee_annu, mois, semaine
+                                                            ORDER BY annee_annu, mois, semaine ) sem_annu_juin_a_mai
+                                                            GROUP BY annee_annu
+                                                            ;
+                                                            """)
 
             """
 TODO: crea vue prenant en compte les jours feries
