68ab2767275df4a67363cd7d8a276673ce904430
corentin leclere <leclere.corentin@gmail.com>
2017-08-31 04:35:25 +0200

    "CLOSED BUG pbNoisclosedElement"

--- dans_mon_coin/exporter_excel.py
+++ dans_mon_coin/exporter_excel.py
@@ -27,13 +27,25 @@ def sauver_excel():
 
     # table partielle :
     # errors: AttributeError: 'Query' object has no attribute 'is_clause_element'
-    p.save_as(session=models.session,
-	  dest_file_name="essai_export_excel.xls",
-	  dest_sheet_name="destination",
-	  table=models.session.query(models.Documents).filter(models.Documents.documents_id == 1).all()
-	  )
+    #q = models.session.query(models.Documents).filter(models.Documents.documents_id==1).all()
+    # error : sqlalchemy.exc.InvalidRequestError: SQL expression, column, or mapped entity expected - got '[Documents(document_id = 1, date_document = 2013-03-01, titre_document = RIB, type_document = ml, origine =,debut_periode_couverte = None,fin_periode_couverte = None]' 
+    #q = models.session.query(models.Documents).filter(models.Documents.documents_id==1)
+    # NameError: name 'documents_id' is not defined
+    # q = models.session.query(models.Documents).filter_by(documents_id == 1)
+    # AttributeError: 'Query' object has no attribute 'is_clause_element'
+    q = models.session.query(models.Documents).filter_by(documents_id = 1)
+    #q = models.session.query(models.Documents).filter_by(documents_id = 1).all()
+    #print(q)
+    p.save_as(
+        #session=models.session,
+        dest_file_name="essai_export_excel.xls",
+        dest_sheet_name="destination",
+        column_names = ['documents_id'], 
+        query_sets = q.all()
+        )
+    
 
-    q = models.session.query(models.Documents).filter(models.Documents.documents_id==1).all()
+    
 
 
     
